201d8af670a2abeab14b8bd032cd908027011895
Dmitrii <razuhindm@gmail.com>
2022-12-06 20:29:20 +0300

    Третья домашняя задача

---
+++ RLE algoritm.py
@@ -0,0 +1,36 @@
+#Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+#Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+path='File1.txt'
+with open(path,'r',encoding='UTF-8') as data:
+    data=data.readline()
+
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+with open('File2.txt','w',encoding='UTF-8') as outputdata:
+    outputdata.write(coding(data)+"\n")
+    outputdata.write(decoding(coding(data)))
